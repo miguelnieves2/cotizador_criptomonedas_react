7d80544a929925dad98374e262287356f70ed989
DevMiguelNieves <devmiguelnieves@gmail.com>
2022-07-30 13:01:45 -0500

    primer commit

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+    {id: 'USD', nombre: 'Dolar Estadounidense'},
+    {id: 'EUR', nombre: 'Euro'},
+    {id: 'COP', nombre: 'Peso Colombiano'},
+    {id: 'GBP', nombre: 'Libra Esterlina'},
+];
+
+export {
+    monedas
+};
